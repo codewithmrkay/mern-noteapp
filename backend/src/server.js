edf80f54bd3b592508c02829e714c043c27d641c
karan <kkadam58707@gmail.com>
2025-11-16 12:22:47 +0530

    mern note app

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,21 +5,31 @@ import { connectDb } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
 import cors from "cors"
-
+import path from "path"
 dotenv.config()
 const port = process.env.PORT || 5001
-
-connectDb().then(() => {
-    app.listen(port, () => {
-        console.log(`visit : http://localhost:${port}`)
-    })
-})
+const __dirname = path.resolve()
 // middleware 
-app.use(cors())
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV != "production") {
+  app.use(cors())
+  app.use(cors({
+    origin: "http://localhost:5173",
+  }));
+}
 app.use(express.json())
 app.use(rateLimiter)
-
 app.use("/api/notes", noteRoute)
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
+
+
+connectDb().then(() => {
+  app.listen(port, () => {
+    console.log(`visit : http://localhost:${port}`)
+  })
+})
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+const BaseUrl = import.meta.env.NODE_ENV=="development" ?"http://localhost:5000/api":"/api"
 const api = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:BaseUrl,
 })
 export default api
